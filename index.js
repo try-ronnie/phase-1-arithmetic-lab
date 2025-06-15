a117478e78fcb9ef440073853f0eee8109d4cad0
ronniegikandi <kiiruronnie42@gmil.com>
2025-06-15 14:41:53 +0300

    less go

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2;
 
-// Write your code here
+let random 
+ random = Math.floor(Math.random()*10)+1;
+
+ let mod
+  let num3 = 10 
+  let num4 = 6
+  mod = num3 % num4
+
+let max
+  max = Math.max(10,20)
